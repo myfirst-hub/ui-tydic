3153a4c31cc84d8250165a2926719df4b75aca5f
天源迪科-宋献峰 <songxianfeng@tydic.com>
2024-10-28 20:10:23 +0800

    去除代码规范校验

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,11 +1,11 @@
 import { Button, ButtonProps } from 'antd';
-import React, { type FC } from 'react';
+import React from 'react';
 
 interface DicButtonProps extends ButtonProps {
   active?: boolean;
 }
 
-const DicButton: FC<DicButtonProps> = (props) => {
+const DicButton = (props: DicButtonProps) => {
   const { active } = props;
   return (
     <Button {...props} style={active ? { borderColor: '#0748df' } : {}}>
